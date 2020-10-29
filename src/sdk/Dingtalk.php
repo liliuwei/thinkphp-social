28dec6c62229b30043e227009a08f40667ead37c
liliuwei <974829947@qq.com>
2020-10-29 17:18:37 +0800

    钉钉登录http方法增加跳过证书检查

--- src/sdk/Dingtalk.php
+++ src/sdk/Dingtalk.php
@@ -69,6 +69,8 @@ class Dingtalk extends Oauth
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
         curl_setopt($ch, CURLOPT_POSTFIELDS,$data_string);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
         curl_setopt($ch, CURLOPT_HTTPHEADER, array(
                 'Content-Type: application/json',
                 'Content-Length: ' . strlen($data_string))
